9960a72c69d5c756ea20157fd778fcbae63352f0
AlexPron <Fotolinza@gmail.com>
2022-04-07 23:25:07 +0300

    lesson1-2

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,30 @@ import React from 'react';
 import './App.css';
 import {TodoList} from "./components/TodoList";
 
+export type TaskType = {
+    id:number
+    title:string
+    isDone:boolean
+}
+
+const tasks1:Array<TaskType> = [
+    {id:1, title:"HTML", isDone:true},
+    {id:2, title:"HTML", isDone:false},
+    {id:3, title:"HTML", isDone:true},
+]
+const tasks2:TaskType[] = [
+    {id:1, title:"HTML", isDone:false},
+    {id:2, title:"HTML", isDone:true},
+    {id:3, title:"HTML", isDone:false},
+]
+
+
 function App() {
     return (
         <div className="App">
-            <TodoList />
-            <TodoList />
-            <TodoList />
+            <TodoList title = {"How to use"} tasks={tasks1} />
+            <TodoList title = {"How to find"} tasks={tasks2}/>
+            <TodoList title = {"How to know"} tasks={tasks1}/>
         </div>
     );
 }
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,9 +1,15 @@
 import React from "react";
+import {TaskType} from "../App";
 
-export const TodoList = () => {
+type TodoListType = {
+    title:string
+    tasks:Array<TaskType>
+}
+
+export const TodoList = (props:TodoListType) => {
     return (
         <div>
-            <h3>What to learn</h3>
+            <h3>{props.title}</h3>
             <div>
                 <input/>
                 <button>+</button>
