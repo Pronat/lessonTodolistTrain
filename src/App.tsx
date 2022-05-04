8652c8157db36f800022d667862d9ea97a572554
Pronat <Fotolinza@gmail.com>
2022-05-04 19:52:19 +0300

    router-dom-2

--- src/App.tsx
+++ src/App.tsx
@@ -1,102 +1,17 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './App.css';
-import {Todolist} from './Todolist';
-import {v1} from 'uuid';
-
-export type FilterValuesType = "all" | "active" | "completed";
-export type TodolistsType = {
-    id: string
-    title: string
-    filter: FilterValuesType
-}
+import { Route } from 'react-router-dom';
 
 function App() {
-
-
-    let todolistID1 = v1();
-    let todolistID2 = v1();
-
-    let [todolists, setTodolists] = useState<Array<TodolistsType>>([
-        {id: todolistID1, title: 'What to learn', filter: 'all'},
-        {id: todolistID2, title: 'What to buy', filter: 'all'},
-    ])
-
-    let [tasks, setTasks] = useState({
-        [todolistID1]: [
-            {id: v1(), title: "HTML&CSS", isDone: true},
-            {id: v1(), title: "JS", isDone: true},
-            {id: v1(), title: "ReactJS", isDone: false},
-            {id: v1(), title: "Rest API", isDone: false},
-            {id: v1(), title: "GraphQL", isDone: false},
-        ],
-        [todolistID2]: [
-            {id: v1(), title: "HTML&CSS2", isDone: true},
-            {id: v1(), title: "JS2", isDone: true},
-            {id: v1(), title: "ReactJS2", isDone: false},
-            {id: v1(), title: "Rest API2", isDone: false},
-            {id: v1(), title: "GraphQL2", isDone: false},
-        ]
-    });
-
-
-    function removeTask(id: string, todolistId: string) {
-        let todolistTask = tasks[todolistId]
-        tasks[todolistId] = todolistTask.filter(t=>t.id !== todolistId)
-        setTasks({...tasks})
-        // setTasks({...tasks, [todolistId]:tasks[todolistId].filter(t=>t.id !== id)})
-    }
-
-    function addTask(todolistId: string, title: string) {
-        let newTask = {id: v1(), title: title, isDone: false};
-        let todolistTask = tasks[todolistId]
-        tasks[todolistId] = [...todolistTask, newTask]
-        setTasks({...tasks})
-        // setTasks({...tasks, [todolistId]:[newTask, ...tasks[todolistId]]})
-    }
-
-    function changeStatus(todolistId: string ,taskId: string, isDone: boolean) {
-        setTasks({...tasks, [todolistId]:[...tasks[todolistId].map(t=>t.id === taskId ? {...t, isDone} : t)]})
-            }
-
-
-
-    // function changeFilter(value: FilterValuesType, todolistId: string) {
-    //   setTodolists(todolists.map(t => t.id === todolistId ? {...t, filter: value} : t))
-    // }
-
-
-    return (
-        <div className="App">
-            {
-                todolists.map(tl => {
-                    // let tasksForTodolist = tasks[tl.id];
-                    //
-                    // if (tl.filter === "active") {
-                    //     tasksForTodolist = tasksForTodolist.filter(t => t.isDone === false);
-                    // }
-                    // if (tl.filter === "completed") {
-                    //     tasksForTodolist = tasksForTodolist.filter(t => t.isDone === true);
-                    // }
-                    return (
-                        <Todolist
-                            setTodolists={setTodolists}
-                            todolists={todolists}
-                            key={tl.id}
-                            todolistId={tl.id}
-                            title={tl.title}
-                            tasks={tasks[tl.id]}
-                            removeTask={removeTask}
-                            // changeFilter={changeFilter}
-                            addTask={addTask}
-                            changeTaskStatus={changeStatus}
-                            filter={tl.filter}
-                        />
-                    )
-                })
-            }
-
-        </div>
-    );
+  return (
+    <div className="App">
+      <h1>Welcome to React Router!</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
