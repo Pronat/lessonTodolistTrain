a2b2cb73cbb91cea9ff44c7b55a40ea1be02dff7
Pronat <Fotolinza@gmail.com>
2022-05-04 21:37:25 +0300

    router-dom-6

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,11 @@ import './App.css';
 import {NavLink, Route, Routes, Outlet, useParams} from 'react-router-dom';
 
 const Profile = () => {
-    const params = useParams()
-
-    const some = params
+    const params = useParams<'*'>()
+    const some = params["*"]
     console.log(some)
     return <div>profile</div>
 }
-
-
 
 function App() {
     return (
@@ -18,16 +15,12 @@ function App() {
             <NavLink to={'/'}>main</NavLink>---
             <NavLink to={'/login'}>login</NavLink>---
             <NavLink to={'/profile/'}>profile</NavLink>---
-            <NavLink to={'/profile/1'}>1</NavLink>
-
-
+            <NavLink to={'/profile/1'}>profile/1</NavLink>
             <Routes>
-                <Route path={"/*"} element={<div>main</div>}/>
+                <Route path={"/*"} element={<div>404</div>}/>
+                <Route path={"/"} element={<div>main</div>}/>
                 <Route path={"/login"} element={<div>login</div>}/>
-                <Route path={"/profile/:id"} element={<Profile/>}/>
-
-
-
+                <Route path={"/profile/*"} element={<Profile/>}/>
             </Routes>
         </div>
     );
