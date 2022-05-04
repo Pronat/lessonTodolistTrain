63cfc4524983643489a661db50d20597a5a076a7
Pronat <Fotolinza@gmail.com>
2022-05-04 20:59:04 +0300

    router-dom-4

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,32 @@
 import React from 'react';
 import './App.css';
-import {NavLink, Route, Routes} from 'react-router-dom';
+import {NavLink, Route, Routes, Outlet} from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-        <NavLink to={'/'}>main</NavLink>---
-        <NavLink to={'/login'}>login</NavLink>---
-        <NavLink to={'/profile/'}>profile</NavLink>---
-        <NavLink to={'/profile/settings'}>settings</NavLink>
+    return (
+        <div className="App">
+            <NavLink to={'/'}>main</NavLink>---
+            <NavLink to={'/login'}>login</NavLink>---
+            <NavLink to={'/profile/'}>profile</NavLink>---
+            <NavLink to={'/profile/settings'}>settings</NavLink>
 
-      <Routes>
-        <Route path="/*" element={<div>main</div>} />
-        <Route path="/login" element={<div>login</div>} />
-        <Route path="/profile/*" element={
-            <div>
-                profile
-                <Routes>
-                    <Route path="/settings" element={<div>settings</div>} />
-                </Routes>
-            </div>
-        } />
-        {/*<Route path="/profile/settings" element={<div>settings</div>} />*/}
-      </Routes>
-    </div>
-  );
+
+            <Routes>
+                <Route path={"/*"} element={<div>main</div>}/>
+                <Route path={"/login"} element={<div>login</div>}/>
+                <Route path={"/profile/"} element={(
+                    <div>
+                        profile
+                        <Outlet/>
+                    </div>
+                )}>
+                    <Route path={"/profile/settings"} element={<div>settings</div>}/>
+                </Route>
+
+
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
