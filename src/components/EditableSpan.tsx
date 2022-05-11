a4d03241a38987a6537c61b04f44df3b26df5fb2
Pronat <Fotolinza@gmail.com>
2022-05-11 21:11:13 +0300

    lesson6-11

--- src/components/EditableSpan.tsx
+++ src/components/EditableSpan.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {ChangeEvent, useState} from 'react';
 
 type EditableSpanPropsType = {
     title: string
@@ -6,12 +6,19 @@ type EditableSpanPropsType = {
 
 export const EditableSpan: React.FC<EditableSpanPropsType> = (props) => {
     let [edit, setEdit] = useState(false)
+    let [title, setTitle] = useState(props.title)
+
     const onDoubleClickHandler = () => {
         setEdit(!edit)
     }
+
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setTitle(e.currentTarget.value)
+    }
+
     return (
         edit
-            ? <input value={props.title}/>
+            ? <input value={props.title} onChange={onChangeHandler}/>
             : <span onDoubleClick={onDoubleClickHandler}>{props.title}</span>
     )
 };
