c03fa08a7376e9d65924db0a47ede0ea4b3f2024
Pronat <Fotolinza@gmail.com>
2022-05-30 18:50:49 +0300

    lesson6-34

--- src/EditableSpan.tsx
+++ src/EditableSpan.tsx
@@ -7,9 +7,10 @@ type EditableSpanPropsType = {
 export const EditableSpan: React.FC<EditableSpanPropsType> = (props) => {
     let [editMode, setEditMode] = useState(false)
     const activateEditMode = () => setEditMode(true)
+    const deactivateEditMode = () => setEditMode(false)
     return (
         editMode
-        ? <input value={props.title}/>
+        ? <input value={props.title} onBlur={deactivateEditMode} autoFocus />
         : <span onDoubleClick={activateEditMode}>{props.title}</span>
     )
 
