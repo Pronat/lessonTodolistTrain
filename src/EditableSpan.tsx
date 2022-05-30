2499860ed85861602c04d35034745e6e056b8976
Pronat <Fotolinza@gmail.com>
2022-05-30 19:34:30 +0300

    lesson6-36

--- src/EditableSpan.tsx
+++ src/EditableSpan.tsx
@@ -2,22 +2,26 @@ import React, {ChangeEvent, useState} from 'react';
 
 type EditableSpanPropsType = {
     title: string
+    onChange: (value: string) => void
    }
 
 export const EditableSpan: React.FC<EditableSpanPropsType> = (props) => {
     let [editMode, setEditMode] = useState(false)
-    let [title, setTitle] = useState(props.title)
+    let [title, setTitle] = useState('')
     const activateEditMode = () => {
         setEditMode(true)
         setTitle(props.title)
     }
-    const deactivateEditMode = () => setEditMode(false)
+    const deactivateEditMode = () => {
+        setEditMode(false)
+        props.onChange(title)
+    }
     const onChangeTitleHandler = (e:ChangeEvent<HTMLInputElement>) => setTitle(e.currentTarget.value)
 
     return (
         editMode
         ? <input value={title} onChange={onChangeTitleHandler} onBlur={deactivateEditMode} autoFocus />
-        : <span onDoubleClick={activateEditMode}>{props.title}--{title}</span>
+        : <span onDoubleClick={activateEditMode}>{props.title}}</span>
     )
 
 };
--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -47,7 +47,7 @@ export function Todolist(props: PropsType) {
 
                     return <li key={t.id} className={t.isDone ? "is-done" : ""}>
                         <input type="checkbox" onChange={onChangeHandler} checked={t.isDone}/>
-                        <EditableSpan title={t.title} />
+                        <EditableSpan title={t.title} onChange={(value)=>alert(value)} />
                         <button onClick={onClickHandler}>x</button>
                     </li>
                 })
