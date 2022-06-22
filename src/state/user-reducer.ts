f26fda9d3d1a85c0213ffccf76fcf08e7610a2ab
Pronat <Fotolinza@gmail.com>
2022-06-22 16:54:13 +0300

    lesson8-3

--- src/state/user-reducer.ts
+++ src/state/user-reducer.ts
@@ -1,4 +1,16 @@
-export const userReducer = (state: any, action: any) => {
+
+type StateType = {
+    age: number
+    childrenCount: number
+    name: string
+}
+
+type ActionType = {
+  type: string
+    [key:string]:any
+}
+
+export const userReducer = (state: StateType, action: any) => {
     switch (action.type) {
         case 'Ver 1':
 
