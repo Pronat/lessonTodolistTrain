1a35cda2713ffa72d1a750d82411f0d4376bb343
Pronat <Fotolinza@gmail.com>
2022-06-22 16:26:32 +0300

    lesson8-2

---
+++ src/state/user-reducer.ts
@@ -0,0 +1,10 @@
+export const userReducer = (state: any, action: any) => {
+    switch (action.type) {
+        case 'Ver 1':
+
+        case 'Ver 2':
+
+        default:
+            throw new Error('I dont understand this action type')
+    }
+}
