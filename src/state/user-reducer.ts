f79ce705cca20cbf10c74efedf8a21d32b2b48ed
Pronat <Fotolinza@gmail.com>
2022-06-25 09:28:56 +0300

    lesson8-25

--- src/state/user-reducer.ts
+++ src/state/user-reducer.ts
@@ -1,37 +1,18 @@
-
 type StateType = {
     age: number
     childrenCount: number
     name: string
 }
-
 type ActionType = {
-  type: string
-    [key:string]:any
+    [key: string]: any
 }
-
-export const userReducer = (state: StateType, action: ActionType): StateType => {
-    switch (action.type) {
-        case 'INCREMENT-AGE':
-            return {
-                ...state,
-                age: state.age + 1
-            }
-            // let newState = {...state}
-            // newState.age = state.age + 1
-            // return newState
-        case 'INCREMENT-CHILDREN-COUNT':
-           return {
-               ...state,
-               childrenCount: state.childrenCount + 1
-           }
-        case 'CHANGE-NAME':
-           return {
-               ...state,
-               name: action.newName
-           }
-
-        default:
-            throw new Error('wrong action type')
-    }
+export const userReducer = (state: StateType, action: ActionType) => {
+   switch (action.type) {
+       case 'INCREMENT-AGE':
+           return state.age + 1
+       case 'INCREMENT-CHOLDREN-COUNT':
+           return state.childrenCount + 1
+       default:
+           throw new Error("We don't understand this type")
+}
 }
