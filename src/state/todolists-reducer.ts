6458dd914ab4492ccaa8288d278befaa0c1965a7
Pronat <Fotolinza@gmail.com>
2022-06-25 13:53:51 +0300

    lesson8-27

---
+++ src/state/todolists-reducer.ts
@@ -0,0 +1,8 @@
+export const todolistsReducer = (state: any, action: any) => {
+    switch (action.type) {
+        case 'XXX':
+            return state
+        default:
+            throw new Error(`Invalid type`)
+    }
+}
