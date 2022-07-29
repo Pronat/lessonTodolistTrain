5da0d364105aee715baf35d95f8ccda50c2b758d
AlexPron <Fotolinza@gmail.com>
2022-07-29 13:29:57 +0300

    lesson10-14

--- src/state/todolists-reducer.ts
+++ src/state/todolists-reducer.ts
@@ -23,7 +23,15 @@ export type ChangeTodolistFilterActionType = {
 
 type ActionsType = RemoveTodolistActionType | AddTodolistActionType | ChangeTodolistTitleActionType | ChangeTodolistFilterActionType;
 
-export const todolistsReducer = (state: Array<TodolistType>, action: ActionsType): Array<TodolistType> => {
+
+let todolistId1 = v1();
+let todolistId2 = v1();
+const initialState: Array<TodolistType> = [
+    {id: todolistId1, title: "What to learn", filter: "all"},
+    {id: todolistId2, title: "What to buy", filter: "all"}
+]
+export const todolistsReducer = (state: Array<TodolistType> = initialState, action: ActionsType): Array<TodolistType> => {
+    debugger
     switch (action.type) {
         case 'REMOVE-TODOLIST':
             return state.filter(tl => tl.id != action.id)
