c9a643b7d211066e4c5e69fbdd0f1c0016511d72
AlexPron <Fotolinza@gmail.com>
2022-07-30 20:01:58 +0300

    lesson10-33

--- src/state/store.ts
+++ src/state/store.ts
@@ -2,14 +2,25 @@ import {combineReducers, createStore, legacy_createStore} from "redux";
 import {tasksReducer} from "./tasks-reducer";
 import {todolistsReducer} from "./todolists-reducer";
 
-const rootReducer = combineReducers({
-    tasks: tasksReducer,
-    todolists: todolistsReducer
+// const rootReducer = combineReducers({
+//     tasks: tasksReducer,
+//     todolists: todolistsReducer
+// })
+//
+// export const store = legacy_createStore(rootReducer)
+//
+// export type AppRootStateType = ReturnType<typeof rootReducer>
+//
+// // @ts-ignore
+// window.store = store
+
+const rootStore = combineReducers({
+    todolists: todolistsReducer,
+    tasks: tasksReducer
 })
 
-export const store = legacy_createStore(rootReducer)
+export const store = legacy_createStore(rootStore)
 
-export type AppRootStateType = ReturnType<typeof rootReducer>
-
+export type AppRootStateType = ReturnType<typeof  rootStore>
 // @ts-ignore
 window.store = store
