592efd5e2b62e95f67674c5d6689d003ec337907
AlexPron <Fotolinza@gmail.com>
2022-07-30 18:41:42 +0300

    lesson10-31

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import AppWithReducer from "./AppWithReducer";
 import AppWithRedux from "./AppWithRedux";
 import {Provider} from "react-redux";
+import {store} from "./state/store";
 
 ReactDOM.render(
     <Provider store={store}>
