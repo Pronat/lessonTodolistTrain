2f79fb1cc0354faa0a2f2feb1bdc19a166efa3c3
AlexPron <Fotolinza@gmail.com>
2022-08-08 16:45:00 +0300

    lesson11-18

--- src/state/tasks-reducer.ts
+++ src/state/tasks-reducer.ts
@@ -67,13 +67,7 @@ export const tasksReducer = (state: TasksStateType = initialState, action: Actio
         }
         case 'CHANGE-TASK-TITLE': {
             let todolistTasks = state[action.todolistId];
-            // найдём нужную таску:
-            let task = todolistTasks.find(t => t.id === action.taskId);
-            //изменим таску, если она нашлась
-            if (task) {
-                task.title = action.title;
-            }
-            state[action.todolistId] = [...todolistTasks]
+           state[action.todolistId] = todolistTasks.map(t => t.id === action.taskId ? {...t, title: action.title} : t)
             return ({...state});
         }
         case 'ADD-TODOLIST': {
