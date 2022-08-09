63ce6952a653a3e711838fe439db832fd3ae948e
AlexPron <Fotolinza@gmail.com>
2022-08-09 20:03:16 +0300

    lesson11-26

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -26,6 +26,7 @@ type PropsType = {
 }
 
 export const Todolist = React.memo((props: PropsType) => {
+    console.log('Todolist')
     const addTask = useCallback((title: string) => {
         props.addTask(title, props.id);
     }, [props.addTask, props.id])
