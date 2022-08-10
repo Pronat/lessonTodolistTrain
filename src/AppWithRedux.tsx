9fca284815c5680833675d71ed6331a21d525ef3
AlexPron <Fotolinza@gmail.com>
2022-08-10 11:48:24 +0300

    lesson11-41

--- src/AppWithRedux.tsx
+++ src/AppWithRedux.tsx
@@ -74,7 +74,7 @@ function AppWithRedux() {
     const addTodolist = useCallback((title: string) => {
         const action = addTodolistAC(title);
         dispatch(action);
-    },[])
+    },[dispatch])
 
     return (
         <div className="App">
@@ -96,15 +96,10 @@ function AppWithRedux() {
                 <Grid container spacing={3}>
                     {
                         todolists.map(tl => {
-                            let allTodolistTasks = tasks[tl.id];
-                            let tasksForTodolist = allTodolistTasks;
+                            // let allTodolistTasks = tasks[tl.id];
+                            // let tasksForTodolist = allTodolistTasks;
 
-                            if (tl.filter === "active") {
-                                tasksForTodolist = allTodolistTasks.filter(t => t.isDone === false);
-                            }
-                            if (tl.filter === "completed") {
-                                tasksForTodolist = allTodolistTasks.filter(t => t.isDone === true);
-                            }
+
 
                             return <Grid item key={tl.id}>
                                 <Paper style={{padding: "10px"}}>
