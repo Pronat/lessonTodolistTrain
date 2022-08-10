1b86cfef8e7479ecd2ecb9524d7251b82fc8c90c
AlexPron <Fotolinza@gmail.com>
2022-08-10 11:21:04 +0300

    lesson11-39

--- src/AddItemForm.tsx
+++ src/AddItemForm.tsx
@@ -6,7 +6,7 @@ type AddItemFormPropsType = {
     addItem: (title: string) => void
 }
 
-export function AddItemForm(props: AddItemFormPropsType) {
+export const AddItemForm = React.memo((props: AddItemFormPropsType) => {
     console.log("AddItemForm")
     let [title, setTitle] = useState("")
     let [error, setError] = useState<string | null>(null)
@@ -47,4 +47,4 @@ export function AddItemForm(props: AddItemFormPropsType) {
             <AddBox />
         </IconButton>
     </div>
-}
+})
