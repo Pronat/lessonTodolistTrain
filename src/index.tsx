3b0cadfef337bcbbed575b77cf38a116d74ef4a4
AlexPron <Fotolinza@gmail.com>
2022-08-12 16:38:09 +0300

    lessonDopi1-17

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import {App} from 'App';
 import reportWebVitals from './reportWebVitals';
 import {Provider, RootStateOrAny} from 'react-redux';
+import {store} from "./store";
 
 ReactDOM.render(
-  <Provider store={{} as RootStateOrAny}>
+  <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('root')
