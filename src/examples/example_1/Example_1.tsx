6f8ee20e5eccc009f6920481981dedfbcd9ce230
AlexPron <Fotolinza@gmail.com>
2022-08-12 22:21:12 +0300

    lessonDopi2-2

--- src/examples/example_1/Example_1.tsx
+++ src/examples/example_1/Example_1.tsx
@@ -1,4 +1,5 @@
-import {ChangeEvent, FC, useCallback, useState} from 'react';
+import React, {ChangeEvent, FC, useCallback, useState} from 'react';
+import {ReactComponent} from "*.svg";
 
 const CONTAINER_STYLES = {display: 'flex', flexDirection: 'column', gap: 10};
 
@@ -8,7 +9,8 @@ type PropsType = { value: string, onChange: (e: ChangeEvent<HTMLInputElement>) =
 // If change value on input Title component not re-render
 // If change value on input Example_1 component not re-render
 
-export const Example_1 = () => {
+export const Example_1 = React.memo(() => {
+  console.log("Example_1")
   const [value, setValue] = useState('');
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -21,9 +23,9 @@ export const Example_1 = () => {
       <Title title="I am a title" />
     </div>
   );
-};
+});
 
-const Title = (props: { title: string }) => <h3>{props.title}</h3>;
+const Title = React.memo((props: { title: string }) => <h3>{props.title}</h3>);
 
 const Input: FC<PropsType> = ({value, onChange}) => {
   return (
