376c83137e98e247acd1d87b1f4d10b13304e653
AlexPron <Fotolinza@gmail.com>
2022-08-12 14:55:57 +0300

    lessonDopi1-10

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import {Provider, RootStateOrAny} from 'react-redux';
 import {store} from "./store";
 
 ReactDOM.render(
-  <Provider store={{store} as RootStateOrAny}>
+  <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('root')
