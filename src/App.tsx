91970dfb87e3b8a3086be92b9bd9df3019f37a3b
AlexPron <Fotolinza@gmail.com>
2022-08-14 13:13:33 +0300

    less1-1

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import { Example_4 } from 'examples/example_4_5_6/Example_4';
-import {Fragment} from 'react';
+import React from 'react';
+import './App.css';
+import {Todolist} from "./Todolist";
 
-export type UserType = {
-  [key: string]: { name: string, id: string }
-};
+function App() {
+    return (
+        <div className="App">
+          <Todolist/>
+          <Todolist/>
+          <Todolist/>
+        </div>
+    );
+}
 
-const DATA_7: UserType[] = [
-  {u1: {name: 'Gleb', id: '1'}},
-  {u2: {name: 'Dima', id: '2'}},
-  {u3: {name: 'Svetlana', id: '3'}},
-  {u4: {name: 'Artur', id: '4'}},
-  {u5: {name: 'Vera', id: '5'}},
-  {u6: {name: 'Vlad', id: '6'}},
-];
+export default App;
 
-export const App = () => {
-  return (
-    <Fragment>
-      <Example_4 />
-      <hr />
-    </Fragment>
-  );
-};
+
