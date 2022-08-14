aa7973f5d256abd6487f39816e88c56eb32bd811
AlexPron <Fotolinza@gmail.com>
2022-08-14 14:41:21 +0300

    les2-1

--- src/App.tsx
+++ src/App.tsx
@@ -9,21 +9,17 @@ export type TasksPropsType = {
 }
 
 function App() {
-    const tasks1: Array<TasksPropsType> = [
+    const tasks: Array<TasksPropsType> = [
         {id: 1, title:"What to learn", isDone: false},
         {id: 2, title:"What to bye", isDone: true},
         {id: 3, title:"What to sell", isDone: false},
-    ]
-    const tasks2: Array<TasksPropsType> = [
-        {id: 1, title:"What to read", isDone: true},
-        {id: 2, title:"What to join", isDone: false},
-        {id: 3, title:"What to fill", isDone: true},
+        {id: 4, title:"rest api", isDone: true},
+        {id: 5, title:"graphQL", isDone: true},
     ]
 
     return (
         <div className="App">
-            <Todolist title={"What to learn"} tasks={tasks1}/>
-            <Todolist title={"What to bye"} tasks={tasks2}/>
+            <Todolist title={"What to learn"} tasks={tasks}/>
         </div>
     );
 }
