702d6936a6103844543c2b8c4b296c33f2dbb676
AlexPron <Fotolinza@gmail.com>
2022-08-17 23:40:31 +0300

    ignat3-14

--- src/p2-homeworks/h3/GreetingContainer.tsx
+++ src/p2-homeworks/h3/GreetingContainer.tsx
@@ -1,9 +1,10 @@
 import React, {useState} from 'react'
 import Greeting from './Greeting'
+import {UserType} from "./HW3";
 
 type GreetingContainerPropsType = {
-    users: any // need to fix any
-    addUserCallback: any // need to fix any
+    users: UserType[] // need to fix any
+    addUserCallback: (name: string) => void // need to fix any
 }
 
 // более простой и понятный для новичков
@@ -12,11 +13,19 @@ type GreetingContainerPropsType = {
 // более современный и удобный для про :)
 // уровень локальной логики
 const GreetingContainer: React.FC<GreetingContainerPropsType> = ({users, addUserCallback}) => { // деструктуризация пропсов
-    const [name, setName] = useState<any>('') // need to fix any
-    const [error, setError] = useState<any>('') // need to fix any
+    const [name, setName] = useState<string>('') // need to fix any
+    const [error, setError] = useState<string>('') // need to fix any
 
     const setNameCallback = (e: any) => { // need to fix any
-        setName('') // need to fix
+        let trimmedName = e.currentTarget.value.trim()
+        if (trimmedName) {
+            setName(trimmedName)
+            setError('')
+        }   else {
+            setName('') // need to fix
+            setError("Title is required")
+        }
+
     }
     const addUser = () => {
         alert(`Hello  !`) // need to fix
