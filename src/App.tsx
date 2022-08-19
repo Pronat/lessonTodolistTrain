b1f5b430803a583db7e7fa1922e43237f0099c71
AlexPron <Fotolinza@gmail.com>
2022-08-19 21:46:39 +0300

    less5-23

--- src/App.tsx
+++ src/App.tsx
@@ -12,19 +12,28 @@ export type TodolistType = {
 
 function App() {
 
+    let todolistId1 = v1()
+    let todolistId2 = v1()
+
     let [todolists, setTodolists] = useState<Array<TodolistType>>([
-        {id: v1(), title: "What to learn", filter: "all"},
-        {id: v1(), title: "What to bye", filter: "all"},
+        {id: todolistId1, title: "What to learn", filter: "all"},
+        {id: todolistId2, title: "What to bye", filter: "all"},
     ])
 
-    let [tasks, setTasks] = useState([
-        {id: v1(), title: "HTML&CSS", isDone: true},
-        {id: v1(), title: "JS", isDone: true},
-        {id: v1(), title: "ReactJS", isDone: false},
-        {id: v1(), title: "Rest API", isDone: false},
-        {id: v1(), title: "GraphQL", isDone: false},
-    ]);
-    // let [filter, setFilter] = useState<FilterValuesType>("all");
+    let [tasks, setTasks] = useState({
+        [todolistId1]: [
+            {id: v1(), title: "HTML&CSS", isDone: true},
+            {id: v1(), title: "JS", isDone: true},
+            {id: v1(), title: "ReactJS", isDone: false},
+        ],
+        [todolistId2]: [
+            {id: v1(), title: "React", isDone: false},
+            {id: v1(), title: "Rest API", isDone: true},
+            {id: v1(), title: "GraphQL", isDone: false},
+        ],
+    })
+
+
 
 
     function removeTask(id: string) {
@@ -46,8 +55,6 @@ function App() {
 
         setTasks([...tasks]);
     }
-
-
 
 
     function changeFilter(todolistId: string, filterValue: FilterValuesType) {
