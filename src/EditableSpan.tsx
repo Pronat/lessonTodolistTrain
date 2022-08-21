3c9a2487201c71674f38143e2241b02d46a5eb3b
AlexPron <Fotolinza@gmail.com>
2022-08-21 14:19:42 +0300

    less6-17

--- src/EditableSpan.tsx
+++ src/EditableSpan.tsx
@@ -6,14 +6,9 @@ export type EditableSpanType = {
 
 
 export const EditableSpan: React.FC<EditableSpanType> = (props) => {
-    const [edit, setEdit] = useState(false)
-    const editHandler = () => {
-       setEdit(!edit)
-    }
+    const [edit, setEdit] = useState("false")
+    return  (
 
-    return (
-        edit
-        ? <input value={props.title}/>
-        : <span onDoubleClick={editHandler}>{props.title}</span>
-    );
-};
+
+    <span>{props.title}</span>
+)}
