f87fe9a1d19feb0ae8a80f5c57675bdca98c3e89
AlexPron <Fotolinza@gmail.com>
2022-08-21 21:51:20 +0300

    less6-29

---
+++ src/EditableSpan.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+type EditableSpanType = {
+    title: string
+}
+
+export const EditableSpan: React.FC<EditableSpanType> = (props) => {
+    return (
+        <span>{props.title}</span>
+    );
+};
