a50c389562035ac7aa2f53ab8bf3210f7ec81b1e
AlexPron <Fotolinza@gmail.com>
2022-08-21 22:12:06 +0300

    less6-30

--- src/EditableSpan.tsx
+++ src/EditableSpan.tsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, {ChangeEvent, useState} from 'react';
 
 type EditableSpanType = {
     title: string
 }
 
 export const EditableSpan: React.FC<EditableSpanType> = (props) => {
+    const [edit, setEdit] = useState(false)
+    let [newTitle, setNewTitle] = useState(props.title)
+
+    const setEditOnHandler = () => {
+        setEdit(true)
+        setNewTitle(props.title)
+    }
+    const setEditOffHandler = () => {
+        setEdit(false)
+    }
+
+    const eventHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setNewTitle(e.currentTarget.value)
+    }
+
     return (
-        <span>{props.title}</span>
+        edit
+        ? <input value={newTitle}
+                 onBlur={setEditOffHandler}
+                 autoFocus={true}
+                 onChange={eventHandler}
+            />
+        : <span onDoubleClick={setEditOnHandler}>{props.title}</span>
     );
 };
