b2ed23ad96bbecbd7c3a113766528bf873e92f61
AlexPron <Fotolinza@gmail.com>
2022-08-23 21:15:32 +0300

    less8-5

--- src/state/user-reducer.test.ts
+++ src/state/user-reducer.test.ts
@@ -4,4 +4,7 @@ test('reducer should increment only age', () => {
     const startState = {age: 20, childrenCount: 4, name: "Aled"}
 
     const endState = userReducer(startState, {type: "INCREMENT-AGE"})
+
+    expect(endState.age).toBe(21)
+    expect(endState.childrenCount).toBe(4)
 })
