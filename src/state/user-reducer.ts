77d1b152bf38b1ac64558f3e65828373c1a07f1d
AlexPron <Fotolinza@gmail.com>
2022-08-23 21:44:32 +0300

    less8-7

--- src/state/user-reducer.ts
+++ src/state/user-reducer.ts
@@ -11,10 +11,11 @@ type ActionType = {
 export const userReducer = (state: StateType, action: ActionType) => {
     switch (action.type) {
         case 'INCREMENT-AGE':
-            state.age = state.age + 1
-            return state
+            let newState = {...state}
+            newState.age = newState.age + 1
         case 'INCREMENT-CHILDREN-COUNT':
-            state.childrenCount = state.childrenCount + 1
+            let newState2 = {...state}
+
         default:
             throw new Error("I don't understand this type")
     }
