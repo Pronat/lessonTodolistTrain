092f285f6221b95dde25105fbbb6c7a5c8dc9303
AlexPron <Fotolinza@gmail.com>
2022-08-24 21:46:56 +0300

    less8-29

--- src/state/todolist-reducer.test.ts
+++ src/state/todolist-reducer.test.ts
@@ -2,10 +2,14 @@ import {useState} from "react";
 import {TodolistType} from "../App";
 import {v1} from "uuid";
 
-let todolistId1 = v1()
-let todolistId2 = v1()
+test('remove todolists should be correct', () => {
+    let todolistId1 = v1()
+    let todolistId2 = v1()
 
-const startState: Array<TodolistType> = [
-    {id: todolistId1, title: "What to learn", filter: "all"},
-    {id: todolistId2, title: "What to buy", filter: "all"}
-]
+    const startState: Array<TodolistType> = [
+        {id: todolistId1, title: "What to learn", filter: "all"},
+        {id: todolistId2, title: "What to buy", filter: "all"}
+    ]
+
+    const endState = todolistReducer(startState, RemoveTodolistTAC)
+})
