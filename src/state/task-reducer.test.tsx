fd6a4ec9002f78e3d05ff018850d08a071147227
AlexPron <Fotolinza@gmail.com>
2022-08-27 19:27:32 +0300

    less9-5

--- src/state/task-reducer.test.tsx
+++ src/state/task-reducer.test.tsx
@@ -1,6 +1,7 @@
 import {useState} from "react";
 import {v1} from "uuid";
 import {TasksStateType, TodolistType} from "../App";
+import {taskReducer} from "./task-reducer";
 
 test('remove task should be correct', () => {
    const startState: TasksStateType = {
@@ -16,6 +17,18 @@ test('remove task should be correct', () => {
         ]
     }
 
-    cons
+    const endState = taskReducer(startState, AC)
+
+    expect(endState).toEqual({
+        "todolistId1": [
+            { id: "1", title: "CSS", isDone: false },
+            { id: "2", title: "JS", isDone: true },
+            { id: "3", title: "React", isDone: false }
+        ],
+        "todolistId2": [
+            { id: "1", title: "bread", isDone: false },
+            { id: "3", title: "tea", isDone: false }
+        ]
+    });
 
 })
