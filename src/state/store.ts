d69212c4c1033379b1c60f8d01a553cdb99c83a7
AlexPron <Fotolinza@gmail.com>
2022-08-28 17:43:48 +0300

    less10-7

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,14 +1,15 @@
-import {combineReducers, createStore, legacy_createStore} from "redux";
+import {combineReducers, legacy_createStore} from "redux";
 import {tasksReducer} from "./tasks-reducer";
 import {todolistsReducer} from "./todolists-reducer";
 
-const rootReducer = combineReducers({
+export const rootReducer = combineReducers({
     tasks: tasksReducer,
     todolists: todolistsReducer
 })
 
 export const store = legacy_createStore(rootReducer)
+
 export type AppRootStateType = ReturnType<typeof rootReducer>
 
 // @ts-ignore
-window.store = store
+windows.store = store
