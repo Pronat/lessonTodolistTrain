4d51a600d1b67dc1b3101a1d8af2fbf0b96d161b
AlexPron <Fotolinza@gmail.com>
2022-08-29 22:47:00 +0300

    less11-8

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -36,9 +36,9 @@ export const Todolist = React.memo((props: PropsType) => {
         props.changeTodolistTitle(props.id, title);
     }
 
-    const onAllClickHandler = () => props.changeFilter("all", props.id);
-    const onActiveClickHandler = () => props.changeFilter("active", props.id);
-    const onCompletedClickHandler = () => props.changeFilter("completed", props.id);
+    const onAllClickHandler = useCallback(() => props.changeFilter("all", props.id),[props.changeFilter, props.id]);
+    const onActiveClickHandler = useCallback(() => props.changeFilter("active", props.id),[props.changeFilter, props.id]);
+    const onCompletedClickHandler = useCallback(() => props.changeFilter("completed", props.id), [props.changeFilter, props.id]);
 
     let allTodolistTasks = props.tasks
 
