7f0ddcc903e1e09c9a4d9ebfb6af955b87d43778
AlexPron <Fotolinza@gmail.com>
2022-08-31 21:02:33 +0300

    less12-12

--- src/stories/Task.stories.tsx
+++ src/stories/Task.stories.tsx
@@ -1,39 +1,29 @@
 import React from 'react';
-import {ComponentStory, ComponentMeta} from '@storybook/react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import {Button} from './Button';
-import {AddItemForm} from "../AddItemForm";
-import {action} from "@storybook/addon-actions";
+import { Button } from './Button';
 import {Task} from "../Task";
 import {TaskType} from "../Todolist";
+import {action} from "@storybook/addon-actions";
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-    title: 'Todolist/Task',
-    component: Task,
-    // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  title: 'Todolist/Task',
+  component: Task,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
 
 } as ComponentMeta<typeof Task>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof Task> = (args) => <Task {...args} />;
 
-export const TaskIsDoneStory = Template.bind({});
+export const TaskisDoneStory = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
-TaskIsDoneStory.args = {
-    changeTaskStatus: action('changeTaskStatus'),
-    changeTaskTitle: action('changeTaskTitle'),
+TaskisDoneStory.args = {
+  changeTaskStatus: action('changeTaskStatus'),
+      changeTaskTitle: action('changeTaskTitle'),
     removeTask: action('removeTask'),
-    task: {id: "werw", title: 'JS', isDone: true},
-    todolistId: 'sdfdef',
+    task: {id: 'sdf', title: 'sfvb', isDone: true},
+todolistId: 'sdfdf'
 };
 
-export const TaskIsNotDoneStory = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
-TaskIsNotDoneStory.args = {
-    changeTaskStatus: action('changeTaskStatus'),
-    changeTaskTitle: action('changeTaskTitle'),
-    removeTask: action('removeTask'),
-    task: {id: "werw", title: 'JS', isDone: false},
-    todolistId: 'sdfdef',
-};
