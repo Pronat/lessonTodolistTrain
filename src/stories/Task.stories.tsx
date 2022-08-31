a2e52909ce27a240876fe53a3f6327aea132857e
AlexPron <Fotolinza@gmail.com>
2022-08-31 21:10:12 +0300

    less12-13

--- src/stories/Task.stories.tsx
+++ src/stories/Task.stories.tsx
@@ -1,29 +1,39 @@
 import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import {ComponentStory, ComponentMeta} from '@storybook/react';
 
-import { Button } from './Button';
+import {Button} from './Button';
 import {Task} from "../Task";
 import {TaskType} from "../Todolist";
 import {action} from "@storybook/addon-actions";
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: 'Todolist/Task',
-  component: Task,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+    title: 'Todolist/Task',
+    component: Task,
+    // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
 
 } as ComponentMeta<typeof Task>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof Task> = (args) => <Task {...args} />;
 
-export const TaskisDoneStory = Template.bind({});
+export const TaskIsDoneStories = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
-TaskisDoneStory.args = {
-  changeTaskStatus: action('changeTaskStatus'),
-      changeTaskTitle: action('changeTaskTitle'),
+TaskIsDoneStories.args = {
+    changeTaskStatus: action('changeTaskStatus'),
+    changeTaskTitle: action('changeTaskTitle'),
     removeTask: action('removeTask'),
-    task: {id: 'sdf', title: 'sfvb', isDone: true},
-todolistId: 'sdfdf'
+    task: {id: 'dfdv', title: 'frfr', isDone: true},
+    todolistId: 'rgr',
+};
+
+export const TaskIsNotDoneStories = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+TaskIsNotDoneStories.args = {
+    changeTaskStatus: action('changeTaskStatus'),
+    changeTaskTitle: action('changeTaskTitle'),
+    removeTask: action('removeTask'),
+    task: {id: 'dfdv', title: 'frfr', isDone: false},
+    todolistId: 'rgr',
 };
 
