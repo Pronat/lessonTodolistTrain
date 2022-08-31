4c04f3e166b8f7fc747b90c763303ce824500b12
AlexPron <Fotolinza@gmail.com>
2022-08-31 20:19:42 +0300

    less12-7

--- src/stories/AddItemForm.stories.tsx
+++ src/stories/AddItemForm.stories.tsx
@@ -3,25 +3,40 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import { Button } from './Button';
 import {AddItemForm} from "../AddItemForm";
-import {action} from "@storybook/addon-actions";
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: 'TODOLIST/AddItemForm',
+  title: 'Todolist/AddItemForm',
   component: AddItemForm,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
-   addItem: {
-     description: 'button clicked inside form'
-   }
+    backgroundColor: { control: 'color' },
   },
-} as ComponentMeta<typeof AddItemForm>;
+} as ComponentMeta<typeof Button>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof AddItemForm> = (args) => <AddItemForm {...args} />;
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 
-export const AddItemFormStory = Template.bind({});
+export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
-AddItemFormStory.args = {
-  addItem: action('button clicked inside form')
+Primary.args = {
+  primary: true,
+  label: 'Button',
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  label: 'Button',
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  size: 'large',
+  label: 'Button',
+};
+
+export const Small = Template.bind({});
+Small.args = {
+  size: 'small',
+  label: 'Button',
 };
