1b82ffefb5cbf3eaac5ce5b777eb2b756619c65e
AlexPron <Fotolinza@gmail.com>
2022-09-01 22:31:28 +0300

    less12-23

--- src/stories/AppWithRedux.stories.tsx
+++ src/stories/AppWithRedux.stories.tsx
@@ -3,6 +3,8 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import { Button } from './Button';
 import AppWithRedux from "../AppWithRedux";
+import {Provider} from "react-redux";
+import {store} from "../state/store";
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
@@ -15,7 +17,7 @@ export default {
 } as ComponentMeta<typeof AppWithRedux>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof AppWithRedux> = (args) => <AppWithRedux {...args} />;
+const Template: ComponentStory<typeof AppWithRedux> = (args) => <Provider store={store}><AppWithRedux/></Provider>;
 
 export const AppWithReduxStory = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
