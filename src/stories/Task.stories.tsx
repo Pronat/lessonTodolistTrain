39fe30a1750c28c2d3df4768a2ecb161708634db
AlexPron <Fotolinza@gmail.com>
2022-09-01 22:01:26 +0300

    less12-18

--- src/stories/Task.stories.tsx
+++ src/stories/Task.stories.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import {ComponentStory, ComponentMeta} from '@storybook/react';
 
-import { Button } from './Button';
+import {Button} from './Button';
 import {Task} from "../Task";
+import {TaskType} from "../Todolist";
+import {action} from "@storybook/addon-actions";
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: 'Todolist/Task',
-  component: Task,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+    title: 'Todolist/Task',
+    component: Task,
+    // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
 
 } as ComponentMeta<typeof Button>;
 
@@ -18,22 +20,9 @@ const Template: ComponentStory<typeof Task> = (args) => <Task {...args} />;
 export const TaskIsDoneStory = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 TaskIsDoneStory.args = {
-
-};
-
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: 'Button',
-};
-
-export const Large = Template.bind({});
-Large.args = {
-  size: 'large',
-  label: 'Button',
-};
-
-export const Small = Template.bind({});
-Small.args = {
-  size: 'small',
-  label: 'Button',
+    changeTaskStatus: action('changeTaskStatus'),
+    changeTaskTitle: action('changeTaskTitle'),
+    removeTask: action('removeTask'),
+    task: {id: "wfef", title: "rbrbbrbr", isDone: true},
+    todolistId: 'dgdgd'
 };
