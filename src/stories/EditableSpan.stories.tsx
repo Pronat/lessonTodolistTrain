dd1ef5eb4550620ca5a62fb783c5e905e310bbdb
AlexPron <Fotolinza@gmail.com>
2022-09-01 22:21:39 +0300

    less12-21

---
+++ src/stories/EditableSpan.stories.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { Button } from './Button';
+import {EditableSpan} from "../EditableSpan";
+import {action} from "@storybook/addon-actions";
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Todolist/EditableSpan',
+  component: EditableSpan,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    onChange: {
+      description: 'Value EditableSpan changed'
+    },
+    value: {
+      defaultValue: "html",
+      description: 'Start Value EditableSpan'
+    }
+
+  },
+} as ComponentMeta<typeof EditableSpan>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof EditableSpan> = (args) => <EditableSpan {...args} />;
+
+export const EditableSpanStory = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+EditableSpanStory.args = {
+  value: '1234',
+  onChange: action('onChange')
+};
+
