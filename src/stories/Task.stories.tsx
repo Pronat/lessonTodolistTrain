a21b021e2e5c624cf7c188b1862680b4430562b7
AlexPron <Fotolinza@gmail.com>
2022-09-01 21:57:56 +0300

    less12-17

--- src/stories/Task.stories.tsx
+++ src/stories/Task.stories.tsx
@@ -1,47 +1,39 @@
-import React, {ChangeEvent, useState} from 'react';
-import {ComponentStory, ComponentMeta} from '@storybook/react';
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import {Button} from './Button';
+import { Button } from './Button';
 import {Task} from "../Task";
-import {TaskType} from "../Todolist";
-import {action} from "@storybook/addon-actions";
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-    title: 'Todolist/Task',
-    component: Task,
-    // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  title: 'Todolist/Task',
+  component: Task,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
 
-  args: {
-    // changeTaskStatus: action('changeTaskStatus'),
-    // changeTaskTitle: action('changeTaskTitle'),
-    // removeTask: action('removeTask'),
-    // todolistId: 'rgr',
-  }
-
-} as ComponentMeta<typeof Task>;
+} as ComponentMeta<typeof Button>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof Task> = (args) => {
-  const [task, setTask] = useState({id: 'dfdv', title: 'frfr', isDone: true})
-  const changeTaskStatus = () => setTask({id: 'dfdv', title: 'frfr', isDone: !task.isDone})
+const Template: ComponentStory<typeof Task> = (args) => <Task {...args} />;
 
-  const changeTaskTitle = (title: string) => {
-    setTask({id: 'dfdv', title: title, isDone: task.isDone})
-  }
+export const TaskIsDoneStory = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+TaskIsDoneStory.args = {
 
-  return <Task changeTaskStatus={changeTaskStatus} changeTaskTitle={changeTaskTitle} removeTask={()=>action('removeTask')} task={task} todolistId={"qqqqq"}/>
-}
+};
 
-// export const TaskIsDoneStories = Template.bind({});
-// // More on args: https://storybook.js.org/docs/react/writing-stories/args
-// TaskIsDoneStories.args = {
-//     todolistId: 'rgr',
-// };
-//
-// export const TaskIsNotDoneStories = Template.bind({});
-// // More on args: https://storybook.js.org/docs/react/writing-stories/args
-// TaskIsNotDoneStories.args = {
-//     task: {id: 'dfdv', title: 'frfr', isDone: false},
-// };
+export const Secondary = Template.bind({});
+Secondary.args = {
+  label: 'Button',
+};
 
+export const Large = Template.bind({});
+Large.args = {
+  size: 'large',
+  label: 'Button',
+};
+
+export const Small = Template.bind({});
+Small.args = {
+  size: 'small',
+  label: 'Button',
+};
