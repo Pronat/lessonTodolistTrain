ccf56bf0206d39b1cc9f9fb3f0bfc43bb0151735
AlexPron <Fotolinza@gmail.com>
2022-09-15 23:37:50 +0300

    analogClock-5

--- src/clock.jsx
+++ src/clock.jsx
@@ -1,63 +1,28 @@
-import * as React from "react";
+import React from 'react';
+import "./styles.css"
 
-export default class Clock extends React.Component {
-    clockInterval = "";
-    constructor(props) {
-        super(props);
-        this.handleDate = this.handleDate.bind(this);
-        this.state = {
-            hours: "",
-            minutes: "",
-            seconds: ""
-        };
-    }
-
-    componentDidMount() {
-        this.clockInterval = setInterval(this.handleDate, 1000);
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.clockInterval);
-    }
-
-    render() {
-        const { hours, minutes, seconds } = this.state;
-        const secondsStyle = {
-            transform: `rotate(${seconds * 6}deg)`
-        };
-        const minutesStyle = {
-            transform: `rotate(${minutes * 6}deg)`
-        };
-        const hoursStyle = {
-            transform: `rotate(${hours * 30}deg)`
-        };
-        const { title } = this.props;
-        return (
-            <div className={"clock"}>
-                <h3>{title}</h3>
-                <div className={"analog-clock"}>
-                    <div className={"dial seconds"} style={secondsStyle} />
-                    <div className={"dial minutes"} style={minutesStyle} />
-                    <div className={"dial hours"} style={hoursStyle} />
-                </div>
-                <div className={"digital-clock"}>
-                    {hours}:{minutes}:{seconds}
-                </div>
+const Clock = () => {
+    const { hours, minutes, seconds } = this.state;
+    const secondsStyle = {
+        transform: `rotate(${Date.getSeconds * 6}deg)`
+    };
+    const minutesStyle = {
+        transform: `rotate(${date.getMinutes * 6}deg)`
+    };
+    const hoursStyle = {
+        transform: `rotate(${date.getHours * 30}deg)`
+    };
+    return (
+        <div className={"clock"}>
+            <h3>Clock</h3>
+            <div className={"analog-clock"}>
+                <div className={"dial seconds"} style={secondsStyle} />
+                <div className={"dial minutes"} style={minutesStyle} />
+                <div className={"dial hours"} style={hoursStyle} />
             </div>
-        );
-    }
+            <div className={"digital-clock"}></div>
+        </div>
+    );
+};
 
-    handleDate() {
-        const { datediff } = this.props;
-        const date = new Date();
-        date.setHours(date.getHours() + datediff);
-        let hours = this.formatTime(date.getHours());
-        let minutes = this.formatTime(date.getMinutes());
-        let seconds = this.formatTime(date.getSeconds());
-        this.setState({ hours, minutes, seconds });
-    }
-
-    formatTime(time) {
-        return time < 10 ? `0${time}` : time;
-    }
-}
+export default Clock;
