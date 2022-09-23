93808b24d8b456de85ff9f8dfdec077dec06d01e
AlexPron <Fotolinza@gmail.com>
2022-09-23 21:47:24 +0300

    lesson13-44

--- src/stories/todolists-api.stories.tsx
+++ src/stories/todolists-api.stories.tsx
@@ -6,7 +6,10 @@ export default {
 }
 
 const settings = {
-    withCredentials: true
+    withCredentials: true,
+    headers: {
+        'API-KEY': "79352066-7263-4742-bb95-6ef08001893f"
+    }
 }
 
 export const GetTodolists = () => {
@@ -26,6 +29,11 @@ export const GetTodolists = () => {
 export const CreateTodolist = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
+        let newTitle = "TraTaTa"
+        let payload = axios.post("https://social-network.samuraijs.com/api/1.1/todo-lists", {title: newTitle}, settings)
+        payload.then( (res) => {
+            setState(res.data)
+        })
     }, [])
 
     return <div>{JSON.stringify(state)}</div>
@@ -33,6 +41,11 @@ export const CreateTodolist = () => {
 export const DeleteTodolist = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
+        let todolistId = "adadb6ed-9be5-4425-b71f-a680fd85c3bb"
+    let promise = axios.delete(`https://social-network.samuraijs.com/api/1.1/todo-lists/${todolistId}`, settings)
+        promise.then( (res) => {
+            setState(res.data)
+        })
     }, [])
 
     return <div>{JSON.stringify(state)}</div>
