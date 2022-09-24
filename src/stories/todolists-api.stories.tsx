05f2f507dabb252e4022f1ffe9a885a3410c56c1
AlexPron <Fotolinza@gmail.com>
2022-09-24 21:47:21 +0300

    lesson13-66

--- src/stories/todolists-api.stories.tsx
+++ src/stories/todolists-api.stories.tsx
@@ -82,8 +82,39 @@ export const GetTasks = () => {
 export const CreateTasks = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
-        let title = "NewTask"
-        let promise = axios.post('https://social-network.samuraijs.com/api/1.1/todo-lists', {title}, settings)
+        let title = "NewEEEEE"
+        let todolistId = '8db9495a-0f67-4193-9bfe-aa2670e5f290'
+        let promise = axios.post(`https://social-network.samuraijs.com/api/1.1/todo-lists/${todolistId}/tasks`, {title}, settings)
+        promise.then((res) => {
+            setState(res.data)
+        })
+    }, [])
+
+    return <div> {JSON.stringify(state)}</div>
+}
+
+export const DeleteTask = () => {
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {
+        let todolistId = '19c6ecc0-318f-450b-8cb9-e55fdc300219'
+        let taskId = '265804bc-e4e8-4bdc-bea5-20ea741f8f80'
+        let promise = axios.delete(`https://social-network.samuraijs.com/api/1.1/todo-lists/${todolistId}/tasks/${taskId}`, settings)
+        promise.then((res) => {
+            setState(res.data)
+        })
+    }, [])
+
+    return <div> {JSON.stringify(state)}</div>
+}
+
+
+export const UpdateTaskTitle = () => {
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {
+        let todolistId = '8db9495a-0f67-4193-9bfe-aa2670e5f290'
+        let taskId = '265804bc-e4e8-4bdc-bea5-20ea741f8f80'
+        let title = "PutTaskTitle"
+        let promise = axios.put(`https://social-network.samuraijs.com/api/1.1/todo-lists/${todolistId}/tasks/${taskId}`,{title}, settings)
         promise.then((res) => {
             setState(res.data)
         })
