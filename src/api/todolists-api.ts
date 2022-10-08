fe21aa96d5baf7559207ed7d4edd9954bec29101
AlexPron <Fotolinza@gmail.com>
2022-10-08 21:55:21 +0300

    lesson 14-3

--- src/api/todolists-api.ts
+++ src/api/todolists-api.ts
@@ -4,7 +4,7 @@ const instance = axios.create({
     baseURL: 'https://social-network.samuraijs.com/api/1.1/',
     withCredentials: true,
     headers: {
-        'API-KEY': '8f2534e2-22a4-4052-894e-a66c04807482'
+        'API-KEY': '79352066-7263-4742-bb95-6ef08001893f'
     }
 })
 
