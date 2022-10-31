e1036e0eeb490cbb28744ce03b2ed0a3064c35e3
AlexPron <Fotolinza@gmail.com>
2022-10-31 22:01:09 +0300

    lesson15-3

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -13,9 +13,13 @@ import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
 import { Menu } from '@mui/icons-material';
 import {LinearProgress} from "@mui/material";
+import {useSelector} from "react-redux";
+import {AppRootStateType} from "./store";
+import {RequestStatusType} from "../features/TodolistsList/app-reducer";
 
 
 function App() {
+    const status = useSelector<AppRootStateType, RequestStatusType>(state => state.app.status)
 
     return (
         <div className="App">
@@ -30,7 +34,10 @@ function App() {
                     <Button color="inherit">Login</Button>
                 </Toolbar>
             </AppBar>
-            <LinearProgress />
+
+            <LinearProgress color={"secondary"}/>
+
+
             <Container fixed>
                 <TodolistsList/>
             </Container>
