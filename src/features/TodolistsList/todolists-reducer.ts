0f1d2ece6ddf753dcfbb7ec13ebf445657c2a679
AlexPron <Fotolinza@gmail.com>
2022-11-06 20:10:56 +0300

    lesson15-25

--- src/features/TodolistsList/todolists-reducer.ts
+++ src/features/TodolistsList/todolists-reducer.ts
@@ -1,6 +1,6 @@
 import {todolistsAPI, TodolistType} from '../../api/todolists-api'
 import {Dispatch} from 'redux'
-import {ActionsAppType, setAppStatusAC} from "./app-reducer";
+import {ActionsAppType, setAppErrorAC, setAppStatusAC} from "./app-reducer";
 
 const initialState: Array<TodolistDomainType> = []
 
@@ -62,8 +62,17 @@ export const addTodolistTC = (title: string) => {
         dispatch(setAppStatusAC('loading'))
         todolistsAPI.createTodolist(title)
             .then((res) => {
-                dispatch(addTodolistAC(res.data.data.item))
-                dispatch(setAppStatusAC('succeeded'))
+                if (res.data.resultCode === 0) {
+                    dispatch(addTodolistAC(res.data.data.item))
+                    dispatch(setAppStatusAC('succeeded'))
+                }   else {
+                    if (res.data.messages.length) {
+                        dispatch(setAppErrorAC(res.data.messages[0]))
+                    }   else {
+                        dispatch(setAppErrorAC('Some error occured'))
+                    }
+                }
+
             })
     }
 }
