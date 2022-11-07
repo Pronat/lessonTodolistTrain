ad90f40b05bb877bec5a6a2183d011cc05d30618
AlexPron <Fotolinza@gmail.com>
2022-11-07 20:18:39 +0300

    lesson15-32

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -13,9 +13,7 @@ import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
 import { Menu } from '@mui/icons-material';
 import {LinearProgress} from "@mui/material";
-import {useSelector} from "react-redux";
-import {AppRootStateType, useAppSelector} from "./store";
-import {RequestStatusType} from "../features/TodolistsList/app-reducer";
+import {useAppSelector} from "./store";
 import {ErrorSnackbar} from "../components/ErrorSnackbar/ErrorSnackbar";
 
 
--- src/features/TodolistsList/Todolist/Todolist.tsx
+++ src/features/TodolistsList/Todolist/Todolist.tsx
@@ -68,7 +68,10 @@ export const Todolist = React.memo(function (props: PropsType) {
                 <Delete />
             </IconButton>
         </h3>
-        <AddItemForm addItem={addTask}/>
+        <IconButton disabled={true}>
+            <AddItemForm addItem={addTask}/>
+        </IconButton>
+
         <div>
             {
                 tasksForTodolist.map(t => <Task key={t.id} task={t} todolistId={props.id}
