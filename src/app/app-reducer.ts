6091cde5764b8d7d8c556c44f54a906bc3a25b22
AlexPron <Fotolinza@gmail.com>
2022-11-12 20:44:13 +0300

    lesson15-71

--- src/app/app-reducer.ts
+++ src/app/app-reducer.ts
@@ -5,7 +5,7 @@ export type RequestStatusType = 'idle' | 'loading' | 'succeeded' | 'failed'
 
 const initialState = {
     status: 'idle' as RequestStatusType,
-    error: "ERROR" as string | null
+    error: null as string | null
 }
 
 type InitialStateType = typeof initialState
--- src/features/TodolistsList/todolists-reducer.ts
+++ src/features/TodolistsList/todolists-reducer.ts
@@ -1,6 +1,6 @@
 import {todolistsAPI, TodolistType} from '../../api/todolists-api'
 import {Dispatch} from 'redux'
-import {AppActionsType, setAppStatusAC} from "../../app/app-reducer";
+import {AppActionsType, setAppErrorAC, setAppStatusAC} from "../../app/app-reducer";
 
 const initialState: Array<TodolistDomainType> = []
 
@@ -62,8 +62,17 @@ export const addTodolistTC = (title: string) => {
         dispatch(setAppStatusAC('loading'))
         todolistsAPI.createTodolist(title)
             .then((res) => {
-                dispatch(addTodolistAC(res.data.data.item))
-                dispatch(setAppStatusAC('succeeded'))
+                if (res.data.resultCode === 0) {
+                    dispatch(addTodolistAC(res.data.data.item))
+                    dispatch(setAppStatusAC('succeeded'))
+                }   else {
+                    if (res.data.messages.length) {
+                        dispatch(setAppErrorAC(res.data.messages[0]))
+                    }   else (
+                        dispatch(setAppErrorAC('Some other error'))
+                    )
+                dispatch(setAppStatusAC('failed'))
+                }
             })
     }
 }
