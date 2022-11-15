92c3e93ef518d9824ef4a2c9a8e6789daee3d1cd
AlexPron <Fotolinza@gmail.com>
2022-11-15 22:53:44 +0300

    lesson16-5

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -14,7 +14,7 @@ import LinearProgress from '@mui/material/LinearProgress';
 import { Menu } from '@mui/icons-material';
 import { ErrorSnackbar } from '../components/ErrorSnackbar/ErrorSnackbar'
 import {Login} from "../features/Login/Login";
-import {Route, Routes} from "react-router-dom";
+import {Navigate, Route, Routes} from "react-router-dom";
 
 type PropsType = {
     demo?: boolean
@@ -41,8 +41,9 @@ function App({demo = false}: PropsType) {
                 <Routes>
                     <Route path = '/' element = {<TodolistsList demo={demo}/>}/>
                     <Route path = '/login' element = {<Login />} />
-                    {/*<Route exact path = '/' render = {() => <TodolistsList demo={demo}/>}/>*/}
-                    {/*<Route exact path = '/login' render = { () => <Login />} />*/}
+
+                    <Route path = '/404' element={<h1>404: PAGE NOT FOUND</h1>} />
+                    <Route path = '/*' element = {<Navigate to='/404' /> } />
                 </Routes>
 
 
