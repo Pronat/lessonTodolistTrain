554fb37250914c02e42a0db7eee39c5dbef4c289
AlexPron <Fotolinza@gmail.com>
2022-11-20 23:02:14 +0300

    lesson16-42

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import './App.css'
-import { TodolistsList } from '../features/TodolistsList/TodolistsList'
-import { useSelector } from 'react-redux'
-import { AppRootStateType } from './store'
-import { RequestStatusType } from './app-reducer'
+import {TodolistsList} from '../features/TodolistsList/TodolistsList'
+import {useSelector} from 'react-redux'
+import {AppRootStateType} from './store'
+import {RequestStatusType} from './app-reducer'
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
@@ -11,10 +11,10 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
 import LinearProgress from '@mui/material/LinearProgress';
-import { Menu } from '@mui/icons-material';
-import { ErrorSnackbar } from '../components/ErrorSnackbar/ErrorSnackbar'
+import {Menu} from '@mui/icons-material';
+import {ErrorSnackbar} from '../components/ErrorSnackbar/ErrorSnackbar'
 import {Login} from "../features/Login";
-import {Route, Routes} from "react-router-dom";
+import {Navigate, Route, Routes} from "react-router-dom";
 
 type PropsType = {
     demo?: boolean
@@ -38,10 +38,11 @@ function App({demo = false}: PropsType) {
                 {status === 'loading' && <LinearProgress/>}
             </AppBar>
             <Container fixed>
-                <TodolistsList demo={demo}/>
                 <Routes>
-                    <Route path='/' element={<TodolistsList demo={demo}/> } />
-                           <Route path = '/login' element={<Login /> } />
+                    <Route path='/' element={<TodolistsList demo={demo}/>}/>
+                    <Route path='/login' element={<Login/>}/>
+                    <Route path='/404' element={<h1>Page not found</h1>}/>
+                    <Route path='/*' element={<Navigate to =  '/404' />}/>
                 </Routes>
             </Container>
         </div>
