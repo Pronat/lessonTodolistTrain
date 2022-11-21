79361e8d1251834d594cdb9da5e7642f1eac0cf8
AlexPron <Fotolinza@gmail.com>
2022-11-21 16:44:52 +0300

    lesson16-45

--- src/features/Login.tsx
+++ src/features/Login.tsx
@@ -14,7 +14,7 @@ export const Login = () => {
         initialValues: {
             email: '',
             password: '',
-            rememberMe: false
+            rememberMe: false,
         },
         onSubmit: values => {
             alert(JSON.stringify(values, null, 2));
@@ -34,26 +34,28 @@ export const Login = () => {
                     <p>Password: free</p>
                 </FormLabel>
                 <FormGroup>
-                    <TextField label="Email"
-                               margin="normal"
-                               name="email"
-                               onChange={formik.handleChange}
-                               value={formik.values.email}
+                    <TextField
+                        label="Email"
+                        margin="normal"
+                        name="email"
+                        onChange={formik.handleChange}
+                        value={formik.values.email}
                     />
-                    <TextField type="password"
-                               label="Password"
-                               margin="normal"
-                               name="password"
-                               onChange={formik.handleChange}
-                               value={formik.values.password}
+                    <TextField
+                        type="password"
+                        label="Password"
+                        margin="normal"
+                        name="password"
+                        onChange={formik.handleChange}
+                        value={formik.values.password}
                     />
-                    <FormControlLabel label={'Remember me'}
-                                      control={<Checkbox/>}
-                                      name="rememberMe"
-                                      onChange={formik.handleChange}
-                                      value={formik.values.rememberMe}
+                    <FormControlLabel
+                        label={'Remember me'}
+                        control={<Checkbox/>}
+                        name="rememberMe"
+                        onChange={formik.handleChange}
+                        checked={formik.values.rememberMe}
                     />
-
                     <Button type={'submit'} variant={'contained'} color={'primary'}>
                         Login
                     </Button>
