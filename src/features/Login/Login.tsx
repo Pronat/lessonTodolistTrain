ff46156ada9dd3f96a7df2966a19eb923dbc3fd1
AlexPron <Fotolinza@gmail.com>
2022-11-23 21:22:28 +0300

    lesson16-68

--- src/features/Login/Login.tsx
+++ src/features/Login/Login.tsx
@@ -9,12 +9,33 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import {useFormik} from "formik";
 
+export type FormikErrorType = {
+    email?: string
+    password?: string
+    rememberMe?: boolean
+}
+
+
 export const Login = () => {
     const formik = useFormik({
         initialValues: {
             email: '',
             password: '',
             rememberMe: false,
+        },
+        validate: (values) => {
+            const errors: FormikErrorType = {}
+            if (!values.email) {
+                errors.email = 'Required'
+            } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+                errors.email = 'Invalid email address'
+            }
+            if (!values.password) {
+                errors.password = 'Required';
+            } else if (values.password.length < 3) {
+                errors.password = 'Must be 3 characters or more';
+            }
+            return errors
         },
         onSubmit: values => {
             alert(JSON.stringify(values, null, 2));
@@ -38,15 +59,25 @@ export const Login = () => {
                     <TextField
                         label="Email"
                         margin="normal"
+                        name="email"
+                        type="email"
+                        onChange={formik.handleChange}
+                        value={formik.values.email}
                     />
                     <TextField
                         type="password"
                         label="Password"
                         margin="normal"
+                        name="password"
+                        onChange={formik.handleChange}
+                        value={formik.values.password}
                     />
                     <FormControlLabel
                         label={'Remember me'}
                         control={<Checkbox/>}
+                        name="rememberMe"
+                        onChange={formik.handleChange}
+                        checked={formik.values.rememberMe}
                     />
                     <Button type={'submit'} variant={'contained'} color={'primary'}>
                         Login
