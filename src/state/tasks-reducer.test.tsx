6488cec15906a8df8208fdad8147b9e2f4b1dfcc
AlexPron <Fotolinza@gmail.com>
2022-11-30 00:04:37 +0300

    lesson9-11

--- src/state/tasks-reducer.test.tsx
+++ src/state/tasks-reducer.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {TasksStateType} from "../App";
-import {addTaskAC, changeTaskStatusAC, removeTaskAC, tasksReducer} from "./tasks-reducer";
+import {addTaskAC, changeTaskStatusAC, changeTitleAC, removeTaskAC, tasksReducer} from "./tasks-reducer";
 
 test('correct task should be deleted from correct array', () => {
     const startState: TasksStateType = {
@@ -77,6 +77,32 @@ test('status of specified task should be changed', () => {
 
     expect(endState["todolistId2"].length).toBe(3)
     expect(endState["todolistId1"].length).toBe(3)
-    expect(endState["todolistId2"][2].isDone).toBe(false)
+    expect(endState["todolistId2"][1].isDone).toBe(false)
     expect(endState["todolistId2"][1].title).toBe('milk')
+})
+
+test('title of specified task should be changed', () => {
+    const startState: TasksStateType = {
+        'todolistId1': [
+            {id: '1', title: 'CSS', isDone: false},
+            {id: '2', title: 'JS', isDone: true},
+            {id: '3', title: 'React', isDone: false}
+        ],
+        'todolistId2': [
+            {id: '1', title: 'bread', isDone: false},
+            {id: '2', title: 'milk', isDone: true},
+            {id: '3', title: 'tea', isDone: false}
+        ]
+    }
+
+    const action = changeTitleAC('todolistId1','1', 'NewTitleName')
+
+    const endState = tasksReducer(startState, action)
+
+    expect(endState["todolistId2"].length).toBe(3)
+    expect(endState["todolistId1"].length).toBe(3)
+    expect(endState["todolistId1"][1].isDone).toBe(true)
+    expect(endState["todolistId1"][0].title).toBe('NewTitleName')
+    expect(endState["todolistId1"][1].title).toBe('JS')
+    expect(endState["todolistId1"][2].title).toBe('React')
 })
--- src/state/tasks-reducer.ts
+++ src/state/tasks-reducer.ts
@@ -1,10 +1,11 @@
 import {TasksStateType} from "../App";
 import {v1} from "uuid";
 
-export type ActionsType = RemoveTaskActionType | ChangeTaskStatusActionType | AddTaskActionType
+export type ActionsType = RemoveTaskActionType | ChangeTaskStatusActionType | AddTaskActionType | ChangeTitleActionType
 type RemoveTaskActionType = ReturnType<typeof removeTaskAC>
 type ChangeTaskStatusActionType = ReturnType<typeof changeTaskStatusAC>
 type AddTaskActionType = ReturnType<typeof addTaskAC>
+type ChangeTitleActionType = ReturnType<typeof changeTitleAC>
 
 
 export const tasksReducer = (state: TasksStateType, action: ActionsType): TasksStateType => {
@@ -22,9 +23,13 @@ export const tasksReducer = (state: TasksStateType, action: ActionsType): TasksS
         case "CHANGE-TASK-STATUS":
             return {
                 ...state,
-                [action.todolistId]: state[action.todolistId].map(el => el.id === action.taskId ? {
-                    ...el, isDone: action.isDone
-                } : el)
+                [action.todolistId]: state[action.todolistId].map(el => el.id === action.taskId ? {...el, isDone: action.isDone}: el)
+            }
+        case "CHANGE-TASK-TITLE":
+            return {
+                ...state,
+                [action.todolistId]: state[action.todolistId].map(el => el.id === action.taskId
+                    ? {...el, title: action.title}: el)
             }
         default:
             return state
@@ -42,9 +47,15 @@ export const changeTaskStatusAC = (todolistId: string, taskId: string, isDone: b
         type: "CHANGE-TASK-STATUS", todolistId, taskId, isDone
     } as const
 }
+export const changeTitleAC = (todolistId: string, taskId: string, title: string) => {
+    return {
+        type: "CHANGE-TASK-TITLE", todolistId, taskId, title
+    } as const
+}
 
 export const addTaskAC = (todolistId: string, title: string) => {
     return {
         type: "ADD-TASK", todolistId, title
     } as const
 }
+
