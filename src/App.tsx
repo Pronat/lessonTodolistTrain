94000a016e0a4d6a60a0fc895556c2863e609ad1
AlexPron <Fotolinza@gmail.com>
2022-11-29 20:51:58 +0300

    lesson9-3

--- src/App.tsx
+++ src/App.tsx
@@ -44,7 +44,7 @@ function App() {
     function removeTask(id: string, todolistId: string) {
         //достанем нужный массив по todolistId:
         let todolistTasks = tasks[todolistId];
-        // перезапишем в этом объекте массив для нужного тудулиста отфилтрованным массивом:
+        // перезапишем в этом объекте массив для нужного тудулиста отфильтрованным массивом:
         tasks[todolistId] = todolistTasks.filter(t => t.id != id);
         // засетаем в стейт копию объекта, чтобы React отреагировал перерисовкой
         setTasks({...tasks});
---
+++ src/state/tasks-reducer.test.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {TasksStateType} from "../App";
+
+test('correct task should be deleted from correct array', () => {
+    const startState: TasksStateType = {
+        'todolistId1': [
+            {id: '1', title: 'CSS', isDone: false},
+            {id: '2', title: 'JS', isDone: true},
+            {id: '3', title: 'React', isDone: false}
+        ],
+        'todolistId2': [
+            {id: '1', title: 'bread', isDone: false},
+            {id: '2', title: 'milk', isDone: true},
+            {id: '3', title: 'tea', isDone: false}
+        ]
+    }
+
+})
