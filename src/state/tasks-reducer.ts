5ef91cfaa84708bca34bd45e4068ffcbd6014794
AlexPron <Fotolinza@gmail.com>
2022-11-29 23:28:59 +0300

    lesson9-9

--- src/state/tasks-reducer.ts
+++ src/state/tasks-reducer.ts
@@ -1,8 +1,10 @@
 import {TasksStateType} from "../App";
+import {v1} from "uuid";
 
-export type ActionsType = RemoveTaskActionType | ChangeTaskStatusActionType
+export type ActionsType = RemoveTaskActionType | ChangeTaskStatusActionType | AddTaskActionType
 type RemoveTaskActionType = ReturnType<typeof removeTaskAC>
 type ChangeTaskStatusActionType = ReturnType<typeof changeTaskStatusAC>
+type AddTaskActionType = ReturnType<typeof addTaskAC>
 
 
 export const tasksReducer = (state: TasksStateType, action: ActionsType): TasksStateType => {
@@ -12,11 +14,16 @@ export const tasksReducer = (state: TasksStateType, action: ActionsType): TasksS
                 ...state,
                 [action.todolistId]: state[action.todolistId].filter(el => el.id !== action.taskId)
             }
+        case "ADD-TASK":
+            return {
+               ...state,
+                [action.todolistId]: [{id: v1(), title: action.title, isDone: false}, ...state[action.todolistId]]
+            }
         case "CHANGE-TASK-STATUS":
             return {
                 ...state,
-                [action.todolistId]: state[action.todolistId].find(el => el.id === action.taskId ? {
-                    ...el, el.id: action.taskId}): el
+                // [action.todolistId]: state[action.todolistId].find(el => el.id === action.taskId ? {
+                //     ...el, el.id: action.taskId}): el
             }
         default:
             return state
@@ -32,5 +39,11 @@ export const removeTaskAC = (todolistId: string, taskId: string) => {
 export const changeTaskStatusAC = (todolistId: string, taskId: string, isDone: boolean) => {
     return {
         type: "CHANGE-TASK-STATUS", todolistId, taskId, isDone
+    } as const
+}
+
+export const addTaskAC = (todolistId: string, title: string) => {
+    return {
+        type: "ADD-TASK", todolistId, title
     } as const
 }
