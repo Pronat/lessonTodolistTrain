da94861dd72240189f6275b2a041054104ca307f
AlexPron <Fotolinza@gmail.com>
2022-11-29 23:04:05 +0300

    lesson9-8

--- src/state/tasks-reducer.test.tsx
+++ src/state/tasks-reducer.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {TasksStateType} from "../App";
-import {removeTaskAC, tasksReducer} from "./tasks-reducer";
+import {changeTaskStatusAC, removeTaskAC, tasksReducer} from "./tasks-reducer";
 
 test('correct task should be deleted from correct array', () => {
     const startState: TasksStateType = {
@@ -30,5 +30,26 @@ test('correct task should be deleted from correct array', () => {
             {id: '3', title: 'tea', isDone: false}
         ]
     })
+})
 
+test('status of specified task should be changed', () => {
+    const startState: TasksStateType = {
+        'todolistId1': [
+            {id: '1', title: 'CSS', isDone: false},
+            {id: '2', title: 'JS', isDone: true},
+            {id: '3', title: 'React', isDone: false}
+        ],
+        'todolistId2': [
+            {id: '1', title: 'bread', isDone: false},
+            {id: '2', title: 'milk', isDone: true},
+            {id: '3', title: 'tea', isDone: false}
+        ]
+    }
+
+    const action = changeTaskStatusAC('todolistId2','2', false)
+
+    const endState = tasksReducer(startState, action)
+
+    expect().toBe()
+    expect().toBe()
 })
--- src/state/tasks-reducer.ts
+++ src/state/tasks-reducer.ts
@@ -1,23 +1,30 @@
 import {TasksStateType} from "../App";
 
-export type ActionsType = RemoveTaskActionType
+export type ActionsType = RemoveTaskActionType | ChangeTaskStatusActionType
+type RemoveTaskActionType = ReturnType<typeof removeTaskAC>
+type ChangeTaskStatusActionType = ReturnType<typeof changeTaskStatusAC>
 
 
 export const tasksReducer = (state: TasksStateType, action: ActionsType): TasksStateType => {
     switch (action.type) {
         case "REMOVE-TASK":
-        return {
-            ...state,
-            [action.todolistId]: state[action.todolistId].filter(el => el.id !== action.taskId)
-        }
+      return {
+         ...state,
+          [action.todolistId]: state[action.todolistId].filter(el => el.id !== action.taskId)
+      }
         default:
             return state
     }
 }
 
-type RemoveTaskActionType = ReturnType<typeof removeTaskAC>
 export const removeTaskAC = (todolistId: string, taskId: string) => {
     return {
         type: 'REMOVE-TASK', todolistId, taskId
     }   as const
+}
+
+export const changeTaskStatusAC = (todolistId: string, taskId: string, isDone: boolean) => {
+    return {
+        type: "CHANGE-TASK-STATUS", todolistId, taskId, isDone
+    }
 }
