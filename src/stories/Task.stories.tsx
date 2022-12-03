1ca630ee7287a47e2a27cf20da61a82ee9045e6e
AlexPron <Fotolinza@gmail.com>
2022-12-03 17:33:39 +0300

    lesson12-5

--- src/stories/Task.stories.tsx
+++ src/stories/Task.stories.tsx
@@ -1,36 +1,36 @@
 import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import {ComponentStory, ComponentMeta} from '@storybook/react';
 
 import {Task} from "../Task";
 import {action} from "@storybook/addon-actions";
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: 'TODOLISTS/Task',
-  component: Task,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+    title: 'TODOLISTS/Task',
+    component: Task,
+    // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
 } as ComponentMeta<typeof Task>;
-
+const baseArgs = {
+    changeTaskStatus: action('changeTaskStatus'),
+    changeTaskTitle: action('changeTaskTitle'),
+    removeTask: action('removeTask'),
+    todolistId: 'todolistId'
+}
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof Task> = (args) => <Task {...args} />;
 
 export const TaskIsDoneStory = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 TaskIsDoneStory.args = {
-    changeTaskStatus: action('changeTaskStatus'),
-    changeTaskTitle: action('changeTaskTitle'),
-    removeTask: action('removeTask'),
+  ...baseArgs,
     task: {id: '1', isDone: true, title: 'JS'},
-    todolistId: 'todolistId'
 };
 
 export const TaskIsNotDoneStory = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 TaskIsNotDoneStory.args = {
-    changeTaskStatus: action('changeTaskStatus'),
-    changeTaskTitle: action('changeTaskTitle'),
-    removeTask: action('removeTask'),
+    ...baseArgs,
     task: {id: '1', isDone: false, title: 'JS'},
-    todolistId: 'todolistId'
+
 };
 
