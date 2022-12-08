41021c94c9e5745b5389029b013035c34d57718f
AlexPron <Fotolinza@gmail.com>
2022-12-08 23:19:16 +0300

    lesson13-3

--- src/stories/todolists-api.stories.tsx
+++ src/stories/todolists-api.stories.tsx
@@ -1,12 +1,26 @@
 import React, {useEffect, useState} from 'react'
+import axios from "axios";
 
 export default {
     title: 'API'
 }
 
+const settings = {
+    withCredentials: true
+}
+
 export const GetTodolists = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
+        const promise = axios.get('https://social-network.samuraijs.com/api/1.1/todo-lists', settings)
+        promise.then((res) => {
+        })
+            .catch((e) => {
+                debugger
+                console.log(e)
+            })
+            .finally(() => {
+            })
         // здесь мы будем делать запрос и ответ закидывать в стейт.
         // который в виде строки будем отображать в div-ке
 
