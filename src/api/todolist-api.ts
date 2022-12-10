7da73c5bff278882b44dd28a9050781eb76a853e
AlexPron <Fotolinza@gmail.com>
2022-12-10 17:33:32 +0300

    lesson13-9

---
+++ src/api/todolist-api.ts
@@ -0,0 +1,21 @@
+
+import axios from 'axios'
+
+const settings = {
+    withCredentials: true,
+    headers: {
+        // Не забываем заменить API-KEY на собственный
+        'API-KEY': '79352066-7263-4742-bb95-6ef08001893f',
+    },
+}
+
+export const todolistAPI = {
+    updateTodolist(todolistId: string, title: string) {
+        const promise = axios.put(
+            `https://social-network.samuraijs.com/api/1.1/todo-lists/${todolistId}`,
+            { title: title },
+            settings
+        )
+        return promise
+    },
+}
