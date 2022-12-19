43bb0fb0bdd330d3f2a7d253ae98b0223efa7f63
AlexPron <Fotolinza@gmail.com>
2022-12-19 21:33:10 +0300

    lesson14-7

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,7 +1,7 @@
 import { tasksReducer } from './tasks-reducer';
 import { todolistsReducer } from './todolists-reducer';
-import {applyMiddleware, combineReducers, createStore} from 'redux';
-import thunk from "redux-thunk";
+import {AnyAction, applyMiddleware, combineReducers, createStore} from 'redux';
+import thunk, {ThunkDispatch} from "redux-thunk";
 
 // объединяя reducer-ы с помощью combineReducers,
 // мы задаём структуру нашего единственного объекта-состояния
@@ -13,6 +13,7 @@ const rootReducer = combineReducers({
 export const store = createStore(rootReducer, applyMiddleware(thunk));
 // определить автоматически тип всего объекта состояния
 export type AppRootStateType = ReturnType<typeof rootReducer>
+export type ThunkAppDispatchType = ThunkDispatch<AppRootStateType, any, AnyAction>
 
 // а это, чтобы можно было в консоли браузера обращаться к store в любой момент
 // @ts-ignore
