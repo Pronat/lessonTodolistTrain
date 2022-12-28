d18694a8b39741a95d7f8b0b8e78893aaf379346
AlexPron <Fotolinza@gmail.com>
2022-12-28 21:45:21 +0300

    lesson14-16

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ import {
     changeTaskStatusAC,
     changeTaskTitleAC,
     removeTaskAC,
-    removeTaskTC
+    removeTaskTC, updateTaskStatusTC
 } from './state/tasks-reducer';
 import {useDispatch, useSelector} from 'react-redux';
 import {AppRootStateType, useAppDispatch} from './state/store';
@@ -52,8 +52,8 @@ function App() {
     }, []);
 
     const changeStatus = useCallback(function (id: string, status: TaskStatuses, todolistId: string) {
-        const action = changeTaskStatusAC(id, status, todolistId);
-        dispatch(action);
+
+        dispatch(updateTaskStatusTC(id, todolistId, status));
     }, []);
 
     const changeTaskTitle = useCallback(function (id: string, newTitle: string, todolistId: string) {
