8d4041c62983c8cd93c5bb6c1616a02bd9a2c034
AlexPron <Fotolinza@gmail.com>
2023-01-12 22:59:30 +0300

    lesson16-11

--- src/features/TodolistsList/Login/Login.tsx
+++ src/features/TodolistsList/Login/Login.tsx
@@ -25,10 +25,16 @@ export const Login = () => {
         },
         validate: (values) => {
             const errors: FormikErrorType = {}
+
             if (!values.email) {
                 errors.email = 'Required'
             } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
                 errors.email = 'Invalid email address'
+            }
+            if (!values.password) {
+                errors.password = 'Required'
+            } else if (values.password.length <=2) {
+                errors.password = 'Must be 3 characters or more'
             }
             return errors
         },
