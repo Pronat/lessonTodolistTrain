72708b726239eef0d283d2599ba9f572f40c894e
AlexPron <Fotolinza@gmail.com>
2023-01-15 22:44:08 +0300

    lesson16-26

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -12,7 +12,7 @@ import Container from '@mui/material/Container';
 import LinearProgress from '@mui/material/LinearProgress';
 import {Menu} from '@mui/icons-material';
 import {ErrorSnackbar} from '../components/ErrorSnackbar/ErrorSnackbar'
-import {Route, Routes} from "react-router-dom";
+import {Navigate, Route, Routes} from "react-router-dom";
 import {Login} from "../features/Login/Login";
 
 
@@ -34,8 +34,11 @@ function App() {
                 {status === 'loading' && <LinearProgress/>}
             </AppBar>
             <Routes>
-                <Route path={'/'} element={<TodolistsList/>} />
+                <Route path={'/'} element={<TodolistsList/> } />
                 <Route path={'/login'} element={<Login/>} />
+                <Route path={'/404'} element={<h1 style={{color: 'red', textAlign: 'center'}}>404: PAGE NOT FOUND</h1>} />
+                <Route path={'*'} element={<Navigate to={'/404'}/>} />
+
             </Routes>
         </div>
     )
