303b456d7f246d19a1f76623a5a1e0ef58a74e63
AlexPron <Fotolinza@gmail.com>
2023-01-19 18:44:11 +0300

    lesson16-36

--- src/features/Login/auth-reducer.ts
+++ src/features/Login/auth-reducer.ts
@@ -1,3 +1,8 @@
+import {LoginParamsType, todolistsAPI} from "../../api/todolists-api";
+import {Dispatch} from "redux";
+import {setAppStatusAC} from "../../app/app-reducer";
+import {handleServerAppError, handleServerNetworkError} from "../../utils/error-utils";
+import {addTaskAC} from "../TodolistsList/tasks-reducer";
 
 
 const initialState = {
@@ -5,13 +10,37 @@ const initialState = {
 }
 
 type InitialStateType = typeof initialState
-type ActionsType = any
+type ActionsType = ReturnType<typeof setIsLoggedInAC>
 
 export const authReducer = (state: InitialStateType = initialState, action: ActionsType) => {
     switch(action.type) {
-        case '1':
+        case 'LOGIN/SET-IS-LOGGED-IN':
             return {
-                ...state, isLoggedIn: action.isLoggedIn
+                ...state, isLoggedIn: action.value
             }
     }
+}
+
+export const setIsLoggedInAC = (value: boolean) => {
+    return {
+        type: 'LOGIN/SET-IS-LOGGED-IN', value
+    } as const
+}
+
+export const setIsLoggedInTC = (data: LoginParamsType) => (dispatch: Dispatch<ActionsType>) => {
+    dispatch(setAppStatusAC('loading'))
+    todolistsAPI.createTask(todolistId, title)
+        .then(res => {
+            if (res.data.resultCode === 0) {
+                const task = res.data.data.item
+                const action = addTaskAC(task)
+                dispatch(setIsLoggedInAC(true))
+            } else {
+                handleServerAppError(res.data, dispatch);
+            }
+        })
+        .catch((error) => {
+            handleServerNetworkError(error, dispatch)
+        })
+
 }
