dacc7fe62c81a9b1a63e824a271249d29d126296
AlexPron <Fotolinza@gmail.com>
2023-02-10 18:27:44 +0300

    lesson2-4

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {Todolist} from './Todolist';
 
 function App() {
 
-    const tasks = [
+    const [tasks,setTasks] = useState([
         { id: 1, title: "HTML&CSS", isDone: true },
         { id: 2, title: "JS", isDone: true },
         { id: 3, title: "ReactJS", isDone: false },
-        { id: 4, title: "React", isDone: false }
-    ]
+        { id: 4, title: "React", isDone: false },
+        { id: 5, title: "Redux", isDone: true }
+    ])
 
 
     const deleteTask = (id: number) => {
-        tasks.filter((el) => el.id !== id)
-        console.log(tasks)
+       let tasks1 = tasks.filter((el) => el.id !== id)
+        setTasks(tasks1)
     }
 
     return (
