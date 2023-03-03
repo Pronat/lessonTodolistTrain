9b973cce8d5d501fa3012342152558b509d800dc
AlexPron <Fotolinza@gmail.com>
2023-03-04 00:13:54 +0300

    less5-33

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import './App.css';
-import {Todolist} from './Todolist';
+import {TaskType, Todolist} from './Todolist';
 import {v1} from 'uuid';
 
 export type FilterValuesType = "all" | "active" | "completed";
@@ -9,7 +9,9 @@ export type TodolistsType = {
     title: string
     filter: FilterValuesType
 }
-
+export type TasksStateType = {
+    [key: string]: Array<TaskType>
+}
 function App() {
 
     let todolistID1 = v1()
@@ -20,7 +22,7 @@ function App() {
         {id: todolistID2, title: 'What to buy', filter: 'all'},
     ])
 
-    let [tasks, setTasks] = useState({
+    let [tasks, setTasks] = useState<TasksStateType>({
         [todolistID1]: [
             {id: v1(), title: 'HTML&CSS', isDone: true},
             {id: v1(), title: 'JS', isDone: true},
