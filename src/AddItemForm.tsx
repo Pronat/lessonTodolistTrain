13d4709636d4735893c3cb651751eff5e51182a9
AlexPron <Fotolinza@gmail.com>
2023-03-08 19:26:59 +0300

    lesson6-2

---
+++ src/AddItemForm.tsx
@@ -0,0 +1,12 @@
+import React, {ChangeEvent, KeyboardEvent, useState} from 'react';
+export type AddItemFormType = {
+
+}
+export const AddItemForm:React.FC<AddItemFormType> = (props) => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
