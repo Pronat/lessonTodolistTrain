f0fd31838d77d4e5ac6b90b974862d66e3be15a9
AlexPron <Fotolinza@gmail.com>
2023-03-09 19:42:13 +0300

    lesson6-12

--- src/EditableSpan.tsx
+++ src/EditableSpan.tsx
@@ -5,15 +5,20 @@ type EditableSpanType = {
 }
 export const EditableSpan: React.FC<EditableSpanType> = ({value}) => {
     const [edit, setEdit] = useState<boolean>(false)
+    const [newTitle, setNewTitle] = useState('')
     const editHandler = () => {
         setEdit(true)
     }
     const editHandlerOnBLur = () => {
         setEdit(false)
     }
+    const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setNewTitle(e.currentTarget.value)
+    }
+
     return (
         edit
-        ? <input value={value} autoFocus={true} onBlur={editHandlerOnBLur}/>
+        ? <input value={newTitle} autoFocus={true} onBlur={editHandlerOnBLur} onChange={onChangeHandler}/>
         : <span onDoubleClick={editHandler}>{value}</span>
     );
 };
