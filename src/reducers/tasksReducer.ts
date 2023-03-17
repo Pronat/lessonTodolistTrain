12dfbcc7dab3324b6c918803ce6a0a887b5c4fd7
AlexPron <Fotolinza@gmail.com>
2023-03-17 23:08:44 +0300

    lesson7-13

--- src/reducers/tasksReducer.ts
+++ src/reducers/tasksReducer.ts
@@ -1,12 +1,19 @@
 import React from 'react';
+import {TaskType} from "../Todolist";
 
-export const TasksReducer = (state: any, action: any) => {
+export const TasksReducer = (state: TaskType[], action: ActionsType): TaskType[] => {
     switch (action.type) {
         case 'XXX': {
             return state
         }
+        default: return state
     }
-
-
 };
 
+type ActionsType = RemoveTaskAT
+type RemoveTaskAT = ReturnType<typeof removeTaskAC>
+export const removeTaskAC = (  ) => {
+    return{
+        type: 'REMOVE-TASK',
+    } as const
+}
