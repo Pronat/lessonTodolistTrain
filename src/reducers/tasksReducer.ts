f6a8158036ad7ff7694d80a175e8da42e01bb246
AlexPron <Fotolinza@gmail.com>
2023-03-17 22:51:36 +0300

    lesson7-12

---
+++ src/reducers/tasksReducer.ts
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const TasksReducer = (state: any, action: any) => {
+    switch (action.type) {
+        case 'XXX': {
+            return state
+        }
+    }
+
+
+};
+
