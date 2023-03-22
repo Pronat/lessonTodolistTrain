0c8933e98a324deab409ba1927747077808bd86e
AlexPron <Fotolinza@gmail.com>
2023-03-22 19:19:57 +0300

    less8-3

---
+++ src/store/user-redcer.test.ts
@@ -0,0 +1,16 @@
+import {userReducer} from "./user-redcer";
+
+
+test('user reducer should increment only age', () => {
+    const startState = {age: 20, childrenCount: 2, name: 'Dimych'}
+
+    const endState = userReducer(startState, {type: 'INCREMENT-AGE'})
+
+    expect(endState.age).toBe(21)
+    expect(endState.childrenCount).toBe(2)
+})
+
+test('user reducer should increment only childrenCount', () => {
+    const startState = {age: 20, childrenCount: 2, name: 'Dimych'}
+    // your code here
+})
